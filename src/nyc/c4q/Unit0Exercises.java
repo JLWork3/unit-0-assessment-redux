d32f3c5152c436dd110217643a4ad656a51e2340
Sea <janneisyvidals@gmail.com>
2015-04-21 21:57:44 -0400

    test 1

--- src/nyc/c4q/Unit0Exercises.java
+++ src/nyc/c4q/Unit0Exercises.java
@@ -5,49 +5,105 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Unit0Exercises {
+public class Unit0Exercises
+{
     /*
      * Name of the method implies what the method should do.
      * Some of these methods require that the method return signature change.
      * For example, `returnPrimitiveBooleanTrue()` should return `boolean`, not `Object`.
      * additional information in the comments of each method
      */
-    public static void main (String args[]) {
+    public static void main(String args[])
+    {
         // Use main to test your methods
         print();
+        System.out.println(returnPrimitiveBooleanFalse());
+        System.out.println(returnPrimitiveInt55Plus44());
+        System.out.println(returnPrimitiveDouble2Point718());
+        System.out.println(returnPrimitiveCharASCII35());
+        System.out.println(isEven(15));
+        System.out.println(isMultipleOfX(1, 3));
+        System.out.println(returnSumOfOddNumbersBetween1And100());
+        System.out.println(returnSumOfTheFirst200MultiplesOf3());
+        System.out.println(isEmptyString(" "));
+        System.out.println(alternateS1AndS2Xtimes("abra", "cadabra", 1));
+
+        //System.out.println(isFromCanada(returnSingers().get("Drake")));
+        System.out.println(stringSplit("ubiquitous","q"));
+        //stringSplit("ubiquitous", "q").equals("ubi") == true
+
+
+
     }
 
-    public static void print() {
+    public static void print()
+    {
         // print out "I'm sorry Dave, I'm afraid I can't let you do that."
-        System.out.println("");
+        System.out.println("I'm sorry Dave, I'm afraid I can't let you do that.");
     }
 
-    public static Object returnPrimitiveBooleanFalse() {
-        return null;
+    public static boolean returnPrimitiveBooleanFalse()
+    {
+        if(1 == 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 
-    public static Object returnPrimitiveInt55Plus44() {
-        return null;
+
+    public static int returnPrimitiveInt55Plus44()
+    {
+        int n = 55;
+        int y = 44;
+        int sum = (55 + 44);
+        return sum;
+
+
     }
 
-    public static Object returnPrimitiveDouble2Point718() {
+
+    public static double returnPrimitiveDouble2Point718()
+    {
         // This is the start of a famous mathematical constant.
         // What's that constant?
-        return null;
+
+        double d = 2.718;
+
+        return d;
     }
 
-    public static Object returnPrimitiveCharASCII35() {
+    public static char returnPrimitiveCharASCII35()
+    {
         // What's the character for the ASCII number 35?
         // Look at an ASCII table and return that character.
         // You can look at the ASCII table at http://www.asciitable.com/
-        return null;
+
+        char c = 35;
+
+        return c;
     }
 
-    public static boolean isEven(int n) {
-        return false;
+    public static boolean isEven(int n)
+    {
+
+        if(n % 2 != 0)
+        {
+
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
-    public static boolean isMultipleOfX(int n, int x) {
+    public static boolean isMultipleOfX(int n, int x)
+    {
         // 0 is a multiple of every number (except 0)
         // For example:
         //      isMultipleOfX(0, 3) == true
@@ -56,26 +112,63 @@ public class Unit0Exercises {
         //      isMultipleOfX(10, 3) == false
         //      isMultipleOfX(28, 7) == true
         // TODO expect a ArithmeticException in Test with x==0
-        return false;
+
+        if(n % x == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    public static int returnSumOfOddNumbersBetween1And100() {
+    public static int returnSumOfOddNumbersBetween1And100()
+    {
         // Starts with [1, 3, 5, ...]
         // Ends with   [..., 95, 97, 99]
-      return 0;
+        int max = 100;
+        int sum = 0;
+
+        for(int i = 1; i < max; i += 2)
+        {
+            sum += i;
+            System.out.println("Sum of odd numbers 1 to 100 : " + sum);
+        }
+        return sum;
     }
 
-    public static int returnSumOfTheFirst200MultiplesOf3() {
+    public static int returnSumOfTheFirst200MultiplesOf3()
+    {
         // Starts with [0, 3, 6, ...]
         // Ends with   [..., 591, 594, 597]
-      return 0;
+        int max = 600;
+        int sum = 0;
+
+            for(int i = 1; i < max; i++)
+            {
+                if(i %3 ==0){
+                sum += i;
+                System.out.println("Sum of odd numbers 1 to 100 : " + sum);
+            }
+        }
+
+        return sum;
     }
 
     public static boolean isEmptyString(String str) {
-        return false;
+
+        if(str.equals("hello")){
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
 
-    public static String alternateS1AndS2Xtimes(String s1, String s2, int x) {
+    public static String alternateS1AndS2Xtimes(String s1, String s2, int x)
+    {
         // Given string `s1`, `s2` and a positive integer `x`,
         // return a string that is equal to s1 + s2 repeated x times.
         // If x is 0 or negative negative, return an empty string `""`.
@@ -84,8 +177,15 @@ public class Unit0Exercises {
         //      alternateS1AndS2Xtimes("abra", "cadabra", 1).equals("abracadabra") == true
         //      alternateS1AndS2Xtimes("abra", "cadabra", 2).equals("abracadabraabracadabra") == true
         //      alternateS1AndS2Xtimes("abra", "cadabra", 3).equals("abracadabraabracadabraabracadabra") == true
+
+
+        for(int i = 1; i <= x; i++)
+        {
+            System.out.println((s1 + s2).equals("abracadabra"));
+        }
         return "";
     }
+
 
     public static String stringSplit(String s, String splitOn) {
         // Given string s, return a string that stops at the first occurrence of "split".
@@ -94,11 +194,17 @@ public class Unit0Exercises {
         //      stringSplit("hellogoodbye", "good").equals("hello") == true
         //      stringSplit("fancy seeing you", "there").equals("") == true
         // If the string does not contain split, then return the empty string "".
+
+        System.out.println(s.split(splitOn).equals("ubi"));
+
         return "";
+
     }
 
     public static Singer returnBeyonce() {
       // should return an instance of Singer with the name "Beyonce" and location "USA"
+
+       // Singer queenB = new Singer()
       return null;
     }
 
@@ -118,11 +224,29 @@ public class Unit0Exercises {
         //      key="Bieber",   Singer(name="Bieber",  location="Canada")
         //      key="Drake",    Singer(name="Drake",   location="Canada")
         //      key="Jepsen",   Singer(name="Jepsen",  location="Canada")
-        return null;
+
+        HashMap<String,Singer> singersList = new HashMap<String, Singer>();
+
+
+//        singersList.put(("Beyonce","USA");
+//        singersList.put("Jay-Z", "USA");
+//        singersList.put("Bieber", "Canada");
+//        singersList.put("Drake", "Canada");
+//        singersList.put("Jepsen", "Canada");
+
+
+        return returnSingers();
     }
 
     public static boolean isFromCanada(Singer person) {
-        return false;
+
+        if(person.getLocation().equals("Canada")){
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 
     public static void changeJayZsLocationToLosAngeles(HashMap<String, Singer> people) {
@@ -143,18 +267,18 @@ public class Unit0Exercises {
 
     }
 
-    // Bonus Problems
-    public static void readFileAndOnlyPrintCanadianCelebrities() {
-        // TODO Use Apache Commons CSV and provide boilerplate for reading the file
-        // https://commons.apache.org/proper/commons-csv/
-      try {
-        FileReader fr = new FileReader("celebrities.csv");
-      } catch (FileNotFoundException e) {
-        e.printStackTrace();
-      }
-    }
-
-    public static void readFileAndPrintCelebrityAges(){
-        // TODO similar as above
-    }
+//    // Bonus Problems
+//    public static void readFileAndOnlyPrintCanadianCelebrities() {
+//        // TODO Use Apache Commons CSV and provide boilerplate for reading the file
+//        // https://commons.apache.org/proper/commons-csv/
+//      try {
+//        FileReader fr = new FileReader("celebrities.csv");
+//      } catch (FileNotFoundException e) {
+//        e.printStackTrace();
+//      }
+//    }
+//
+//    public static void readFileAndPrintCelebrityAges(){
+//        // TODO similar as above
+//    }
 }
